2b3303aa168b41c7e1a2c2a59b33ea4bd3c5e31a
Abdul-Samad-17 <abdulsammadsardar17@gmail.com>
2026-01-14 08:29:02 +0500

    configuratioin and uploading files on claoudinary

---
+++ src/utils/claudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLAUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+    // Uploading
+    const UploadOnCloudinary = async (localFilePath) => {
+        try {
+            if(!localFilePath) {
+                console.log("Local file path not found !!!");
+                return null 
+            }
+            
+            // upload the file on claudinary
+            const response = await cloudinary.uploader.upload(localFilePath, {
+                resource_type: "auto"
+            })
+            // file has been uploaded successfully
+            console.log("File is uploaded on claodinary. ", response.url);
+            return response;
+
+        } catch (error) {
+            fs.unlink(localFilePath); // Unlinking files from local 
+            // storage.
+            console.log("Error: ", error);
+            return null
+        }
+    }
